ff87fcc303b84f5b14b6ba971e9c5adb0de5eec1
jiwon010106 <jjiwon753@gmail.com>
2024-12-20 16:28:51 +0900

    test1

--- index.js
+++ index.js
@@ -56,16 +56,16 @@ app.post("/chat", async (req, res) => {
     const { question } = req.body;
 
     // 추가 함
-    const pythonPath = path.join(
-      "/home/ubuntu/miniconda",
-      "envs",
-      "myenv",
-      "bin",
-      "python3"
-    );
-    const scriptPath = path.join(__dirname, "chatbot.py");
+    // const pythonPath = path.join(
+    //   "/home/ubuntu/miniconda",
+    //   "envs",
+    //   "myenv",
+    //   "bin",
+    //   "python3"
+    // );
+    const scriptPath = path.join(__dirname, "chatbot", "chatbot.py");
 
-    const result = spawn(pythonPath, [scriptPath, question]);
+    // const result = spawn(pythonPath, [scriptPath, question]);
 
     const phythonPath = path.join(
       "/home/ubuntu/miniconda",
@@ -76,10 +76,10 @@ app.post("/chat", async (req, res) => {
     );
 
     let answer = "";
-    let hasResponse = false;
+    // let hasResponse = false;
 
     // Python 스크립트 실행
-    // const pythonProcess = spawn("python", [scriptPath, question]);
+    const pythonProcess = spawn(phythonPath, [scriptPath, question]);
 
     pythonProcess.stdout.on("data", (data) => {
       answer += data.toString();
